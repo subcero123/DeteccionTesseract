82d4e06a98634dc13bb177ba7ae9019d41654110
Julio cesar alvarez galicia <64234453+JulioCesarAlvarezGalicia@users.noreply.github.com>
2023-10-18 18:32:00 -0600

    Update OCR.py
    nueva version de OCR

--- OCR/OCR.py
+++ OCR/OCR.py
@@ -1,7 +1,6 @@
 import cv2
 import pytesseract
-import os
 image = cv2.imread('imagen2.jpg')
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-text = pytesseract.image_to_string(gray)
-print(text)
+texto = pytesseract.image_to_string(gray)
+print(texto)
