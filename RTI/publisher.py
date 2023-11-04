431b6e30d421ea5f8c639e5d3744b309361896a5
subcero123 <subcero139@hotmail.com>
2023-11-03 19:46:05 -0600

    RTI

--- RTI/publisher.py
+++ RTI/publisher.py
@@ -1,30 +1,37 @@
-#Encargado de la segmentacion del texto
-#Orden de ejecución, publisher, reconocimiento, postprocesamiento, suscriber. 
 from time import sleep
-
+import rticonnextdds_connector as rti
 from sys import path as sys_path
 from os import path as os_path
+from OCRfinal import *
 
 file_path = os_path.dirname(os_path.realpath(__file__))
 sys_path.append(file_path + "/../../../")
 
-import rticonnextdds_connector as rti
+# Abrir el archivo PDF y leer su contenido binario
+with open('../aPdf/1.pdf', 'rb') as pdf_file:
+    pdf_content = pdf_file.read()
 
 with rti.open_connector(
-    config_name="MyParticipantLibrary::SegmenterParticipant",
+    config_name="MyParticipantLibrary::MyPubParticipant",
     url=file_path + "/MyAppConfig.xml"
 ) as connector:
 
-    output = connector.get_output("SegmenterPublisher::SegmenterWriter")
+    output = connector.get_output("MyPublisher::MyTextPublisher")
 
-    print("Waiting for subscribers...")
+    print("Waiting for subscriptions...")
     output.wait_for_subscriptions()
 
-    print("Writing segmented text...")
-    segmented_texts = ["Segment 1 of text...", "Segment 2 of text...", "Segment 3 of text..."]
+    pdf_filename = '1.pdf'
+    parrafo = obtenerParrafo(pdf_filename)
+    parrafo = " ".join(parrafo)
 
-    for segment in segmented_texts:
-        output.instance.set_string("segment", segment)
+    print("Writing...")
+    for i in range(1, 2):
+        recognized_text = parrafo  # Aquí define el texto que deseas enviar
+        output.instance.set_string("texto", recognized_text)
         output.write()
 
-        sleep(0.5)
+        sleep(0.5)  # Escribe a una velocidad de un mensaje cada 0.5 segundos, por ejemplo.
+
+    print("Exiting...")
+    output.wait()  # Espera a que todas las suscripciones reciban los datos antes de salir
--- RTI/suscriber.py
+++ RTI/suscriber.py
@@ -1,6 +1,8 @@
-#Responsable de unir el PDF
 from __future__ import print_function
+from OCRfinal import *
 
+# Updating the system path is not required if you have pip-installed
+# rticonnextdds-connector
 from sys import path as sys_path
 from os import path as os_path
 
@@ -10,20 +12,22 @@ sys_path.append(file_path + "/../../../")
 import rticonnextdds_connector as rti
 
 with rti.open_connector(
-    config_name="MyParticipantLibrary::PDFGeneratorParticipant",
+    config_name="MyParticipantLibrary::MySubParticipant",
     url=file_path + "/MyAppConfig.xml"
 ) as connector:
 
-    input = connector.get_input("PDFGeneratorSubscriber::PDFGeneratorReader")
+    input = connector.get_input("MySubscriber::MyTextReader")
 
-    print("Waiting for subscriptions...")
-    input.wait_for_publications()
+    print("Waiting for publications...")
+    input.wait_for_publications()  # Espera al menos una publicación coincidente
 
-    print("Waiting for processed text...")
+    print("Waiting for data...")
     for i in range(1, 500):
-        input.wait()
+        input.wait()  # Espera datos en esta entrada
         input.take()
         for sample in input.samples.valid_data_iter:
-            segmento_recibido = sample.get_string("segment")
-            # Realiza la generación de PDF con segmento_recibido
-            # Puedes utilizar bibliotecas como PyPDF2 o ReportLab para esto
+            # Obtén el campo 'texto' del mensaje
+            texto_recibido = sample.get_string("texto")
+            print(texto_recibido)
+            texto_recibido = texto_recibido.split('\n')
+            generarOutput(texto_recibido, "texto_final.pdf")
